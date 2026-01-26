cb10d896ab96166d8ca3d1f4afa7da504b161ad8
OpenClocking <openclocking@geoffrey.driat.fr>
2026-01-26 22:21:20 +0100

    updated default module index (login, password change)

--- modules/default/index.mjs
+++ modules/default/index.mjs
@@ -1,6 +1,11 @@
 export const name = "default"
 import express from 'express'
 
+import { messageManager } from '../../utils.mjs'
+
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+
 const router = express.Router()
 
 router.get("/", async function(req, res) {
@@ -14,11 +19,144 @@ router.get("/", async function(req, res) {
 })
 
 router.get("/login", async function(req, res) {
-    const [settingsDb] = await global.db.query(`SELECT * FROM settings WHERE name = "lang" OR name = "company_name"`)
+    const [settingsDb] = await global.db.query(`
+        SELECT * FROM settings WHERE 
+        name = "lang" OR 
+        name = "company_name" OR 
+        name = "company_domain" OR 
+        name = "first_login" OR 
+        name = "default_password"`)
     const settings = Object.fromEntries(settingsDb.map(({ name, value }) => [name, value]));
-    res.render(`./modules/${name}/views/login.ejs`, {settings})
+    res.render(`./modules/${name}/views/login.ejs`, {settings, messages : {}})
 })
 
-console.log(router.stack.map(function(stack) { return { route : stack.route?.path, methods : Object.keys(stack.route?.methods || {})}}))
+router.post("/login", async function(req, res) {
+    const [settingsDb] = await global.db.query(`
+        SELECT * FROM settings WHERE 
+        name = "lang" OR 
+        name = "company_name" OR 
+        name = "company_domain" OR 
+        name = "first_login" OR 
+        name = "default_password" OR 
+        name = "jwt_key"`)
+    const settings = Object.fromEntries(settingsDb.map(({ name, value }) => [name, value]));
+    let messages = {}
+    if(req.body.email && req.body.password) {
+        const [user] = await global.db.query("SELECT id, email, password_hash, change_password FROM users where email = ?", [req.body.email])
+        if(user.length && await bcrypt.compare(req.body.password, user[0].password_hash)) {
+            const token = jwt.sign({user_id : user[0].id, change_password : user[0].change_password, remember : req.body.remember ? true : false}, settings.jwt_key)
+            res.cookie("token", token, {httpOnly : true, maxAge : req.body.remember ? 1 * 24 * 60 * 60 * 1000 : undefined})
+
+            if(user[0].change_password) {
+                res.redirect("/change_password")
+                return
+            } else {
+                res.redirect("/")
+                return
+            }
+        } else {
+            messageManager(messages, "login", `
+                <div class="alert alert-danger" role="alert">
+                    ${global.i18n.translate(settings.lang, "errors_login_credentials")}
+                </div>`)
+        }  
+    } else {
+        messageManager(messages, "login", `
+            <div class="alert alert-danger" role="alert">
+                ${global.i18n.translate(settings.lang, "errors_login_missing_fields")}
+            </div>`)
+    }
+    res.render(`./modules/${name}/views/login.ejs`, {settings, messages})
+})
+
+router.get("/change_password", async function(req, res) {
+    if(!req.user) {
+        res.redirect("/login")
+        return
+    }
+    const [settingsDb] = await global.db.query(`
+        SELECT * FROM settings WHERE 
+        name = "lang" OR 
+        name = "company_name" OR 
+        name = "first_login" OR 
+        name = "default_password" OR 
+        name = "company_domain"`)
+    const settings = Object.fromEntries(settingsDb.map(({ name, value }) => [name, value]));
+    res.render(`./modules/${name}/views/change_password.ejs`, {settings, messages : {}})
+})
+
+router.post("/change_password", async function(req, res) {
+    if(!req.user) {
+        res.redirect("/login")
+        return
+    }
+    const [settingsDb] = await global.db.query(`
+        SELECT * FROM settings WHERE 
+        name = "lang" OR 
+        name = "company_name" OR 
+        name = "company_domain" OR 
+        name = "first_login" OR 
+        name = "default_password" OR 
+        name = "jwt_key" OR 
+        name = "bcrypt_salt_rounds" OR 
+        name = "anti_password_recycle_count"`)
+    const settings = Object.fromEntries(settingsDb.map(({ name, value }) => [name, value]));
+    let messages = {}
+    if(req.body.old_password && req.body.password && req.body.password_repeat) {
+        if(await bcrypt.compare(req.body.old_password, req.user.password_hash)) {
+            if(req.body.password === req.body.password_repeat) {
+                let password_recycled = false
+                for(const password_hash of JSON.parse(req.user.password_history)) {
+                    if(await bcrypt.compare(req.body.password, password_hash)) {
+                        password_recycled = true
+                        break;
+                    }
+
+                }
+                if(!password_recycled) {
+                    const bcrypt_hash = await bcrypt.hash(req.body.password, parseInt(settings.bcrypt_salt_rounds))
+                    await global.db.execute(`UPDATE users SET password_hash = ?, change_password = false, password_history = ? WHERE id = ?`, 
+                    [
+                        bcrypt_hash,
+                        JSON.stringify([bcrypt_hash, ...JSON.parse(req.user.password_history)].slice(0, parseInt(settings.anti_password_recycle_count || 1))),
+                        req.user.id
+                    ])
+                    if(req.user.id === 1 && parseInt(settings.first_login)) {
+                        await global.db.execute(`UPDATE settings SET value = 0 WHERE name = "first_login"`)
+                    }
+                    const token = jwt.sign({user_id : req.user.id, remember : req.user.session.remember}, settings.jwt_key)
+                    res.cookie("token", token, {httpOnly : true, maxAge : req.user.session.remember ? 1 * 24 * 60 * 60 * 1000 : undefined})
+                    res.redirect("/")
+                    return;
+                } else {
+                    messageManager(messages, "change_password", `
+                        <div class="alert alert-danger" role="alert">
+                            ${global.i18n.translate(settings.lang, "errors_change_password_recycled", {count : settings.anti_password_recycle_count || 1})}
+                        </div>`)
+                }
+            } else {
+                messageManager(messages, "change_password", `
+                    <div class="alert alert-danger" role="alert">
+                        ${global.i18n.translate(settings.lang, "errors_change_passwords_mismatch")}
+                    </div>`)
+            }
+        } else {
+            messageManager(messages, "change_password", `
+                <div class="alert alert-danger" role="alert">
+                    ${global.i18n.translate(settings.lang, "errors_change_password_wrong_old_password")}
+                </div>`)
+        }
+    } else {
+         messageManager(messages, "change_password", `
+            <div class="alert alert-danger" role="alert">
+                ${global.i18n.translate(settings.lang, "errors_change_password_missing_fields")}
+            </div>`)
+    }
+
+    res.render(`./modules/${name}/views/change_password.ejs`, {settings, messages})
+})
+
+
+//console.log(router.stack.map(function(stack) { return { route : stack.route?.path, methods : Object.keys(stack.route?.methods || {})}}))
 
 global.app.use(router)
