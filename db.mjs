277eba0e122d177457e22115cd2edbc322776e38
OpenClocking <openclocking@geoffrey.driat.fr>
2026-01-26 22:19:26 +0100

    added more default values to settings, added password (hash) history to prevent password recycling

--- db.mjs
+++ db.mjs
@@ -34,11 +34,16 @@ async function createTables() {
         await global.db.execute(`INSERT INTO settings (name, value) VALUES ("db_version", "1")`)
         await global.db.execute(`INSERT INTO settings (name, value) VALUES ("lang", "en_us")`)
         await global.db.execute(`INSERT INTO settings (name, value) VALUES ("bcrypt_salt_rounds", "13")`)
+        await global.db.execute(`INSERT INTO settings (name, value) VALUES ("jwt_key", ?)`, [randomBytes(12).toString("hex")])
         await global.db.execute(`INSERT INTO settings (name, value) VALUES ("default_password", "changeme")`)
+        await global.db.execute(`INSERT INTO settings (name, value) VALUES ("anti_password_recycle_count", "10")`)
         await global.db.execute(`INSERT INTO settings (name, value) VALUES ("internal_id_required", "0")`)
-        await global.db.execute(`INSERT INTO settings (name, value) VALUES ("default_permissions", ?)`, [permissions.CHANGE_EMAIL, permissions.CHANGE_PASSWORD])
+        await global.db.execute(`INSERT INTO settings (name, value) VALUES ("default_permissions", ?)`, [permissions.CHANGE_EMAIL + permissions.CHANGE_PASSWORD])
         await global.db.execute(`INSERT INTO settings (name, value) VALUES ("mifare_key", ?)`, [randomBytes(6).toString("hex")])
         await global.db.execute(`INSERT INTO settings (name, value) VALUES ("company_name", "OpenClocking")`)
+        await global.db.execute(`INSERT INTO settings (name, value) VALUES ("company_domain", "openclocking.com")`)
+        await global.db.execute(`INSERT INTO settings (name, value) VALUES ("service_url", "https://demo.openclocking.com")`)
+        await global.db.execute(`INSERT INTO settings (name, value) VALUES ("first_login", "1")`)
     }
     if(!tables.includes("users")) {
         await global.db.execute(`
@@ -49,12 +54,14 @@ async function createTables() {
             	display_name TEXT NULL,
                 email varchar(320) NULL,
                 password_hash varchar(72) NULL,
+                password_history TEXT NULL,
             	permissions BIGINT DEFAULT 0 NOT NULL,
             	internal_id varchar(100) NULL,
             	admin_only TINYINT(1) DEFAULT 0 NOT NULL,
                 change_password TINYINT(1) DEFAULT 0 NOT NULL,
             	CONSTRAINT users_pk PRIMARY KEY (id),
-            	CONSTRAINT users_unique UNIQUE KEY (internal_id)
+            	CONSTRAINT users_unique UNIQUE KEY (internal_id),
+                CONSTRAINT email_unique UNIQUE KEY (email)
             )
             ENGINE=InnoDB
             DEFAULT CHARSET=utf8mb4
@@ -63,12 +70,13 @@ async function createTables() {
         const settings = Object.fromEntries(
             settingsDb.map(({ name, value }) => [name, value])
         );
-        await global.db.execute(`INSERT INTO users (display_name, email, password_hash, permissions, admin_only, change_password) VALUES (?, ?, ?, ?, ?, ?)`, 
+        const password_bcrypt = await bcrypt.hash(settings.default_password, parseInt(settings.bcrypt_salt_rounds))
+        await global.db.execute(`INSERT INTO users (display_name, email, password_hash, password_history, permissions, admin_only, change_password) VALUES (?, ?, ?, ?, ?, ?, ?)`, 
             [
                 "OpenClocking Manager",
                 "manager@openclocking.com",
-                await bcrypt.hash(settings.default_password,
-                parseInt(settings.bcrypt_salt_rounds)),
+                password_bcrypt,
+                JSON.stringify([password_bcrypt]),
                 permissions.ALL,
                 1,
                 1
